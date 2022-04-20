b424f2f2cd52e7bbf8f672faedaa0aebddea267a
Les Aker <me@lesaker.org>
2022-04-19 22:28:54 -0400

    handle boot device

--- manifests/systemd.pp
+++ manifests/systemd.pp
@@ -11,6 +11,18 @@ class disks::systemd () {
     pass    => '1',
   }
 
+  if $facts['partitions']['/dev/mmcblk0p1'] {
+    mounttab { '/':
+      ensure  => present,
+      device  => '/dev/mmcblk0p1',
+      atboot  => true,
+      fstype  => 'vfat',
+      options => ['defaults'],
+      dump    => '0',
+      pass    => '0',
+    }
+  }
+
   resources { 'mounttab':
     purge => true,
   }
