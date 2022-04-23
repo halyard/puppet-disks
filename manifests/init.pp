57220b5e20343ac6ca7d5c1e8e5709c60f23cc6f
Les Aker <me@lesaker.org>
2022-04-23 09:37:26 -0400

    magic mounts change

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,8 @@
 # @summary Configure devices and mounts
 #
+# @param lvs devices to manage
 class disks (
+  Hash[String, Hash] $lvs = [],
 ) {
   case $facts['os']['family'] {
     'Archlinux': { include disks::systemd }
--- manifests/systemd.pp
+++ manifests/systemd.pp
@@ -1,29 +1,29 @@
 ##
 # Definitions for systemd
 class disks::systemd () {
-  mounttab { '/':
+  mount { '/':
     ensure  => present,
     device  => $facts['mountpoints']['/']['device'],
     atboot  => true,
     fstype  => $facts['mountpoints']['/']['filesystem'],
-    options => ['rw', 'relatime'],
+    options => 'rw,relatime',
     dump    => '0',
     pass    => '1',
   }
 
   if $facts['partitions']['/dev/mmcblk0p1'] {
-    mounttab { '/boot':
+    mount { '/boot':
       ensure  => present,
       device  => '/dev/mmcblk0p1',
       atboot  => true,
       fstype  => 'vfat',
-      options => ['defaults'],
+      options => 'defaults',
       dump    => '0',
       pass    => '0',
     }
   }
 
-  resources { 'mounttab':
+  resources { 'mount':
     purge => true,
   }
 }
